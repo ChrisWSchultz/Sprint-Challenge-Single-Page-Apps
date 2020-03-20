fb379ac1cb39ca29f33e11b7a4664aafaafa6b36
Christopher W Schultz <chris.william.schultz@gmail.com>
2020-03-20 17:27:04 -0600

    pulling data okay

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [ characters, setCharacters ] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    Axios
+        .get('https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/')
+        .then((response) => {
+          setCharacters(response.data.results);
+        });
+
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+        {characters.map((character) => {
+            return (
+                <h2 key={ character.id }>{ character.name }</h2>
+            );
+        })}
     </section>
   );
 }
